62783e7356b5d705859eda567c9c51a2a9ef3ccf
René Buffat <buffat@gmail.com>
2020-06-09 18:54:36 +0200

    update kt gl

--- tests/test_kt_gl.py
+++ tests/test_kt_gl.py
@@ -11,8 +11,8 @@ def wms_kt_gl():
 
 @pytest.mark.parametrize("layer_name,layer_title", [
     ("ch.gl.imagery.orthofoto2013", "Luftbild Orthofoto 2013"),
-    ("ch.gl.imagery.orthofoto2015", "Digitales Terrainmodell 50cm, Relief"),
-    ("ch.gl.imagery.orthofoto2017", "Digitales Terrainmodell 50cm, Relief"),
+    ("ch.gl.imagery.orthofoto2015", "Luftbild Orthofoto 2015"),
+    ("ch.gl.imagery.orthofoto2017", "Luftbild Orthofoto 2017"),
 ])
 @pytest.mark.filterwarnings('ignore:.*Content metadata for layer*')
 def test_layer(layer_name, layer_title, wms_kt_gl):
